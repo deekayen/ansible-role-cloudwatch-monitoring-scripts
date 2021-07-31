a468654490ba3c7d28ee5515ddecfcb10da98561
David Norman <deekayen@deekayen.net>
2021-07-31 02:13:45 -0400

    Add some testinfra tests for Molecule.

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,21 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_dependencies_installed(host):
+    assert host.package("crontabs").is_installed
+    assert host.package("zip").is_installed
+    assert host.package("unzip").is_installed
+
+
+def test_monitor_file(host):
+    assert host.file("/opt/aws-scripts-mon").is_directory
+
+    path = "/opt/aws-scripts-mon/mon-put-instance-data.pl"
+    file = host.file(path)
+    assert file.exists
+    assert file.is_file
+    assert file.size > 0
